8b5c2bf118f60f15b883baff62413d52225842d0
Munira12345 <munirangomo@gmail.com>
2025-08-12 12:18:34 +0300

    Initial commit

---
+++ app/src/main/java/com/example/ecoconverterapp/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.ecoconverterapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.ecoconverterapp.ui.theme.EcoConverterAppTheme
+import androidx.compose.material3.Surface
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            EcoConverterAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    EcoConverterScreen()
+                }
+            }
+        }
+
+    }
+
+    @Composable
+    fun EcoConverterScreen() {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        )
+        {
+            Text("Hey, welcome to Eco Converter", style = MaterialTheme.typography.headlineSmall)
+        }
+    }
+
+
+    @Preview(showBackground = true)
+    @Composable
+    fun EcoConverter() {
+        EcoConverterAppTheme {
+            EcoConverterScreen()
+        }
+    }
+}
