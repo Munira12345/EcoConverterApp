0fbe0dd1e449b64dd80ae7657a5eb1ca39c88e8a
Munira12345 <munirangomo@gmail.com>
2025-08-26 21:47:06 +0300

    defined imports for the main screen of the app

--- app/src/main/java/com/example/ecoconverterapp/ui/screens/EcoScreen.kt
+++ app/src/main/java/com/example/ecoconverterapp/ui/screens/EcoScreen.kt
@@ -1,1 +1,24 @@
 package com.example.ecoconverterapp.ui.screens
+
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+//import androidx.compose.material.icons.filled.Description
+//import androidx.compose.material.icons.filled.Image
+//import androidx.compose.material.icons.filled.PictureAsPdf
+//import androidx.compose.material.icons.filled.TextSnippet
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+//background can be very light green , might change once i fix the UI
+val LightGreen = Color(0xFFE8F5E9)
