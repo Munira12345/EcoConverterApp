411cbaa782ed72ca71574ac7347ab55c6133243e
Munira12345 <munirangomo@gmail.com>
2025-08-29 08:58:05 +0300

    defined the box with the initial text of our eco screen

--- app/src/main/java/com/example/ecoconverterapp/ui/screens/EcoScreen.kt
+++ app/src/main/java/com/example/ecoconverterapp/ui/screens/EcoScreen.kt
@@ -1,6 +1,15 @@
 package com.example.ecoconverterapp.ui.screens
 
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 //import androidx.compose.material.icons.filled.Description
@@ -22,3 +31,28 @@ import androidx.compose.ui.unit.sp
 
 //background can be very light green , might change once i fix the UI
 val LightGreen = Color(0xFFE8F5E9)
+@Composable
+fun EcoScreen(){
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(LightGreen),
+             contentAlignment = Alignment.Center
+    ){
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.padding(16.dp)
+        ){
+            Text(
+                text = "choose your file",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Black
+            )
+
+            Spacer(modifier = Modifier.height(32.dp))
+        //to define a row with icons and a reusable composable with text
+        }
+    }
+}
