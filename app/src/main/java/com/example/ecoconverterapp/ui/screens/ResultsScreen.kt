e39033cac685b55b51dab16f2aff9c9fe414998a
Munira12345 <munirangomo@gmail.com>
2025-09-16 09:49:09 +0300

    imports for the results screen

--- app/src/main/java/com/example/ecoconverterapp/ui/screens/ResultsScreen.kt
+++ app/src/main/java/com/example/ecoconverterapp/ui/screens/ResultsScreen.kt
@@ -1,2 +1,20 @@
 package com.example.ecoconverterapp.ui.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Download
+import androidx.compose.material.icons.filled.Image
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.ecoconverterapp.ui.theme.EcoConverterAppTheme
+
+
